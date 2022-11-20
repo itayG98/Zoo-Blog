63b396680478641763de168457890041059ddf4e
GEITAY5@365.openu.ac.il <GEITAY5@365.openu.ac.il>
2022-11-20 14:05:43 +0200

    Add 10MB validation in server side

--- Model/Models/Attributes/ImageFileValidationAttribute.cs
+++ Model/Models/Attributes/ImageFileValidationAttribute.cs
@@ -10,10 +10,13 @@ namespace Model.Models.Attributes
 {
     public class ImageFileValidationAttribute : ValidationAttribute
     {
+        const int Max_File_Size = 10 * 1024 * 1024; // 10MB
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
             if (value is IFormFile file != default)
             {
+                if (file.Length > Max_File_Size)
+                    return new ValidationResult("This file's size is bigger than the 10MB limitation");
                 if (file.ContentType.Contains("image"))
                     return ValidationResult.Success;
                 return new ValidationResult("This is not a valid file ");
