16e07aa62266e69e5705d3675ccc1fd84de2961a
GEITAY5@365.openu.ac.il <GEITAY5@365.openu.ac.il>
2022-11-23 17:56:01 +0200

    !

--- Model/DAL/ZooIdentityContext.cs
+++ Model/DAL/ZooIdentityContext.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace Model.DAL
 {
@@ -15,10 +16,32 @@ namespace Model.DAL
         {
         }
 
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            
+            builder.Entity<IdentityRole>().HasData(new List<IdentityRole>
+            {
+                new IdentityRole() {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+            },
+                new IdentityRole() {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "User",
+                    NormalizedName = "USER"
+                  },
+                });
+            IdentityRole adminRole = new IdentityRole() { Name = "Admin" };
+            IdentityUser admin = new IdentityUser()
+            {
+                UserName = "Itay",
+                Email = "Stam@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "1234567890",
+                PhoneNumberConfirmed = true
+            };
         }
     }
 }
