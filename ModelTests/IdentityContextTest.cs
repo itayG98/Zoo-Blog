08d253a177ba0ace4a38682d893faaa4e664e169
GEITAY5@365.openu.ac.il <GEITAY5@365.openu.ac.il>
2022-11-24 16:41:08 +0200

    Ignore of testing which not setteled

--- ModelTests/IdentityContextTest.cs
+++ ModelTests/IdentityContextTest.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Model.DAL;
+using Moq;
 
 namespace ModelTests
 {
-    [Ignore("No suitable SetUpp")]
+    [Ignore("No suitable set Up of IdentityContext userManager and SignInMananager")]
     public class IdentityContextTest
     {
         private ZooIdentityContext _identityContext;
         private readonly string? _connString;
-        private  UserManager<IdentityUser>? _userManager;
-        private  SignInManager<IdentityContextTest>? _signInManager;
+        private UserManager<IdentityUser>? _userManager;
+        private SignInManager<IdentityContextTest>? _signInManager;
         private const string PASSWORD = "1234ABcd@";
         private readonly IdentityUser user = new IdentityUser
         {
@@ -20,25 +21,26 @@ namespace ModelTests
         };
 
         [SetUp]
-        //public void Setup()
-        //{
-        //    string _connString = "Data Source=IdentityMockDB.db;";
-        //    var optionsBuilder = new DbContextOptionsBuilder<ZooIdentityContext>();
-        //    optionsBuilder.UseSqlite(_connString);
-        //    _identityContext = new ZooIdentityContext(optionsBuilder.Options);
-        //    if (_identityContext != default)
-        //    {
-        //        _identityContext.Database.EnsureDeleted();
-        //        _identityContext.Database.EnsureCreated();
-        //        _userManager = new UserManager<IdentityUser>();
-        //        _signInManager = new SignInManager<IdentityContextTest>();
-        //    }
-        //    else
-        //        throw new Exception("Repositories were not set");
-        //}
+        [Ignore("No suitable set Up of IdentityContext userManager and SignInMananager")]
+        public void Setup()
+        {
+            string _connString = "Data Source=IdentityMockDB.db;";
+            var optionsBuilder = new DbContextOptionsBuilder<ZooIdentityContext>();
+            optionsBuilder.UseSqlite(_connString);
+            _identityContext = new ZooIdentityContext(optionsBuilder.Options);
+            if (_identityContext != default)
+            {
+                _identityContext.Database.EnsureDeleted();
+                _identityContext.Database.EnsureCreated();
+                var _userManager = new Mock<UserManager<IdentityUser>>();
+                var _signInManager = new Mock<SignInManager<IdentityContextTest>>();
+            }
+            else
+                throw new Exception("Repositories were not set");
+        }
 
         [Test]
-        [Ignore("No suitable SetUpp")]
+        [Ignore("No suitable set Up of IdentityContext userManager and SignInMananager")]
         public async Task Register()
         {
             var createResult = await _userManager.CreateAsync(user, PASSWORD);
